d2944cfab96bb25a5340bc094a5cf7909be1bf1d
Avishai Ish-Shalom <avishai.ish-shalom@mail.huji.ac.il>
2011-03-23 15:19:31 +0200

    Added cores hash

--- Doat/recipes/webapp.rb
+++ Doat/recipes/webapp.rb
@@ -7,11 +7,15 @@ end
 app_config = data_bag_item(:doat_config, :webapp)
 sql = search(:endpoints, "type:rds AND db:#{app_config["db"]}").first
 sql_credentials = search(:credentials, "usage:db_#{app_config["db"]}").first
+cores = all_providers_for_service(:core).reduce({}) do |cores_hash, core|
+  cores_hash[core[:ipaddress]] = core[:doat][:core][:port]
+  cores_hash
+end
 
 template "/opt/doat/www/app/config/local.config.php" do
   source "webapp-local.config.php.erb"
   notifies :restart, "service[php-cgi]" if node[:php][:apc][:stat] == 0
-  variables :sql => sql, :sql_credentials => sql_credentials, :app_config => app_config
+  variables :sql => sql, :sql_credentials => sql_credentials, :app_config => app_config, :cores => cores
 end
 
 template ::File.join(node[:nginx][:dir], "sites-enabled", "doat-webui") do
