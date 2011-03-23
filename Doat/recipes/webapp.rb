ff6541bb63cd32e8a4a579613f636849f0a6598e
Avishai Ish-Shalom <avishai.ish-shalom@mail.huji.ac.il>
2011-03-23 14:32:13 +0200

    Fixed nginx conf dir, app_config keys

--- Doat/recipes/webapp.rb
+++ Doat/recipes/webapp.rb
@@ -5,8 +5,8 @@ include_recipe "Doat::webserver_common"
 end
 
 app_config = data_bag_item(:doat_config, :webapp)
-sql = search(:endpoints, "type:rds AND db:#{app_config[:db]}").first
-sql_credentials = search(:credentials, "usage:db_#{app_config[:db]}").first
+sql = search(:endpoints, "type:rds AND db:#{app_config["db"]}").first
+sql_credentials = search(:credentials, "usage:db_#{app_config["db"]}").first
 
 template "/opt/doat/www/app/config/local.config.php" do
   source "webapp-local.config.php.erb"
@@ -14,12 +14,12 @@ template "/opt/doat/www/app/config/local.config.php" do
   variables :sql => sql, :sql_credentials => sql_credentials, :app_config => app_config
 end
 
-template ::File.join(node[:nginx][:conf_dir], "sites-enabled", "doat-webui") do
+template ::File.join(node[:nginx][:dir], "sites-enabled", "doat-webui") do
   source "nginx-webui.conf.erb"
   notifies :reload, "service[nginx]"
 end
 
-template ::File.join(node[:nginx][:conf_dir], "sites-enabled", "doat-developer") do
+template ::File.join(node[:nginx][:dir], "sites-enabled", "doat-developer") do
   source "nginx-developer.conf.erb"
   notifies :reload, "service[nginx]"
 end
