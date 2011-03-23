556b34a11c46819be5162af3ae9de06ea466ee6c
Avishai Ish-Shalom <avishai.ish-shalom@mail.huji.ac.il>
2011-03-23 19:11:43 +0200

    Added scrib.port to default attributes

--- Doat/attributes/scribe.rb
+++ Doat/attributes/scribe.rb
@@ -1,6 +1,7 @@
 default[:scribe][:tmp_dir]  = "/mnt/scribe_tmp"
 default[:scribe][:user]     = "scribe"
 default[:scribe][:group]    = "scribe"
+default[:scribe][:port]     = 1463
 default[:scribe][:conf_dir] = "/etc/scribe"
 default[:scribe][:daemon]   = case kernel[:machine]
                               when "i686", "i386"
