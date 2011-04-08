af686324c5bcae057be9a6c14b993ca7f7db218f
Avishai Ish-Shalom <avishai.ish-shalom@mail.huji.ac.il>
2011-04-08 16:26:44 +0300

    Added support for remote_user attribute

--- ssh/recipes/default.rb
+++ ssh/recipes/default.rb
@@ -9,25 +9,44 @@ service "ssh" do
   action [:start, :enable]
 end
 
-admin_keys = []
-users = []
+user_keys = {}
+root_keys = []
 admin_groups = node[:ssh][:admin_groups] || [ "admins" ]
-admin_groups_query = admin_groups.map do |grp|
-  "groups:#{grp}"
-end.join(" OR ")
-search("users", "(#{admin_groups_query}) AND ssh_public_key:[* TO *]").each do |user|
-  admin_keys.push user['ssh_public_key'].chomp
-  users.push user['id']
+search("users", "ssh_public_key:[* TO *]").each do |user|
+  user_name = nil
+  user_name = user["remote_user"] if user.has_key? "remote_user"
+  if (user["groups"] & admin_groups).any?
+    root_keys << user["ssh_public_key"]
+  end
+  unless user_name.nil?
+    user_keys[user_name] = 
+      user_keys.fetch(user_name, Array.new).push(user["ssh_public_key"].chomp)
+  end
 end
-Chef::Log.info "Adding SSH keys for #{users.join(", ")}" if users.any?
+
 ruby_block "Manage root's authorized_keys" do
   block do
     ::File.open("/root/.ssh/authorized_keys", "a+") do |f|
-      keys_to_add = admin_keys - f.readlines
+      keys_to_add = root_keys - f.readlines.map{|l| l.chomp}
       keys_to_add.each do |public_key|
         f.puts public_key
       end
     end
+  end
+end
+
+Chef::Log.info "SSH KEYS: " + user_keys.inspect
+user_keys.each do |user, keys|
+  auth_keys_file = begin
+    ::File.join(node[:etc][:passwd][user][:dir], ".ssh", "authorized_keys")
+                   rescue
+                     "/home/#{user}/.ssh/authorized_keys"
+                   end
+  file auth_keys_file do
+    content keys.join('\n')
+    owner user
+    mode "0700"
+    ignore_failure true
   end
 end
 
