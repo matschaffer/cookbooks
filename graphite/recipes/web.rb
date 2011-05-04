051e04fef70160cd9a715df5cded844a505afce7
Avishai Ish-Shalom <avishai.ish-shalom@mail.huji.ac.il>
2011-05-04 19:59:38 +0300

    Minor fixes for web recipe

--- graphite/recipes/web.rb
+++ graphite/recipes/web.rb
@@ -4,28 +4,30 @@ include_recipe "graphite::common"
 if node[:graphite][:install_flavor] == "source"
   include_recipe "graphite::web_source"
 else
-  template "/etc/apache2/sites-available/graphite" do
-    source "graphite-vhost.conf.erb"
-  end
+  package "graphite"
+end
 
-  template ::File.join(node[:graphite][:webapp_dir], "graphite", "graphite.wsgi")
+template "/etc/apache2/sites-available/graphite" do
+  source "graphite-vhost.conf.erb"
+end
 
-  apache_site "graphite"
+template ::File.join(node[:graphite][:webapp_dir], "graphite", "graphite.wsgi")
 
-  directory ::File.join(node[:graphite][:log_dir], "webapp") do
-    owner node[:apache][:user]
-    group node[:apache][:group]
-  end
+apache_site "graphite"
 
-  directory node[:graphite][:storage_dir] do
-    owner node[:apache][:user]
-    group node[:apache][:group]
-  end
+directory ::File.join(node[:graphite][:log_dir], "webapp") do
+  owner node[:apache][:user]
+  group node[:apache][:group]
+end
 
-  cookbook_file ::File.join(node[:graphite][:storage_dir], "graphite.db") do
-    owner node[:apache][:user]
-    group node[:apache][:group]
-    action :create_if_missing
-  end
+directory node[:graphite][:storage_dir] do
+  owner node[:apache][:user]
+  group node[:apache][:group]
+end
+
+cookbook_file ::File.join(node[:graphite][:storage_dir], "graphite.db") do
+  owner node[:apache][:user]
+  group node[:apache][:group]
+  action :create_if_missing
 end
 provide_service("graphite-web")
