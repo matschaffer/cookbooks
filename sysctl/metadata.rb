2b73d0200c90443d4dadd3b36bfbdad6353f1305
Mat Schaffer <mat@schaffer.me>
2011-11-03 13:33:54 -0300

    Switch license to Apache 2.0
    @tovbinm said it was cool to use this. And I like your implementation better than the @37signals one.

--- sysctl/metadata.rb
+++ sysctl/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Fewbytes Technologies"
 maintainer_email "avishai@fewbytes.com"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Configures sysctl parameters"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.2"
